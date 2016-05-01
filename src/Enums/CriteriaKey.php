08f9cbd10717e5f6df5f85ab36023cb865536dd5
Coen Zimmerman <coen@pixelindustries.com>
2016-05-01 12:20:40 +0200

    switched out phpextra enum for myclabs enum

--- src/Enums/CriteriaKey.php
+++ src/Enums/CriteriaKey.php
@@ -1,37 +1,16 @@
 <?php
 namespace Czim\Repository\Enums;
 
-use PHPExtra\Type\Enum\Enum;
+use MyCLabs\Enum\Enum;
 
 /**
  * Unique identifiers for standard Criteria that may be loaded in repositories.
  */
 class CriteriaKey extends Enum
 {
-    const DEFAULT_VALUE = '';
-
     const ACTIVE = 'active';    // whether to check for 'active' = 1
     const CACHE  = 'cache';     // for rememberable()
     const ORDER  = 'order';     // for order by (multiple in one optionally)
     const SCOPE  = 'scope';     // for scopes applied (multiple in one optionally)
     const WITH   = 'with';      // for eager loading
-
-
-    /**
-     * Returns list of all constants defined (without _default)
-     *
-     * @return array
-     */
-    public function getList()
-    {
-        $reflect   = new \ReflectionClass(get_class($this));
-        $constants = $reflect->getConstants();
-
-        if (array_key_exists('DEFAULT_VALUE', $constants)) {
-            unset($constants['DEFAULT_VALUE']);
-        }
-
-        return $constants;
-    }
-
 }
