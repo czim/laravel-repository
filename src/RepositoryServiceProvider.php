44873add337572721e40a45f13b025b60328e823
Danny Janse <d.janse@live.com>
2016-07-14 19:05:37 +0200

    Repository Generator

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -6,11 +6,43 @@ use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
+	/**
+     * Commands provided by the current package.
+     *
+     * @var array
+     */
+    private $commands = [
+        \Czim\Repository\Console\Commands\RepositoryMakeCommand::class,
+    ];
+
+    /**
+     * The base package path.
+     *
+     * @var string
+     */
+    public static $packagePath = null;
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
+        self::$packagePath = dirname(dirname(__DIR__));
+
+        $this->publishes([
+            self::$packagePath . '/config/repository.php' => config_path('repository.php'),
+        ], 'repository');
     }
 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
+        $this->commands($this->commands);
     }
 }
